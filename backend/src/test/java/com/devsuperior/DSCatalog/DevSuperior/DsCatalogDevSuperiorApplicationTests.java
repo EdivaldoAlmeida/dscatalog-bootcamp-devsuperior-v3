5f1b6ea25d751d53cd3767f5fcf1114f5a483adc
Edivaldo Almeida <edivaldo.va@gmail.com>
2022-09-27 14:58:15 -0300

    Backend project created

---
+++ backend/src/test/java/com/devsuperior/DSCatalog/DevSuperior/DsCatalogDevSuperiorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.DSCatalog.DevSuperior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsCatalogDevSuperiorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
