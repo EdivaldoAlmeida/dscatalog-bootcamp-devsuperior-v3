bd9aa171ce2208d969305c9498a47711cc60bfa7
Edivaldo Almeida <edivaldo.va@gmail.com>
2022-09-29 05:53:17 -0300

    DTO

--- backend/src/main/java/com/devsuperior/DSCatalog/DevSuperior/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/DSCatalog/DevSuperior/services/CategoryService.java
@@ -1,22 +1,37 @@
 package com.devsuperior.DSCatalog.DevSuperior.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.DSCatalog.DevSuperior.dto.CategoryDTO;
 import com.devsuperior.DSCatalog.DevSuperior.entities.Category;
 import com.devsuperior.DSCatalog.DevSuperior.repositories.CategoryRepository;
 
 @Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		
+		/*Todo este código abaixo foi substituído pela linha acima (expressão lãmbida)
+		 * cuja função é transformar uma lista de objetos do tipo Category para CatetoryDTO
+		 * com o intuito de atender as especificações da arquiterura, na qual apenas os
+		 * objetos do tipo CategoryDTO devem ser movimentados entre as camadas
+		List<CategoryDTO> listDTO = new ArrayList<>();
+		for (Category cat : list) {
+			listDTO.add(new CategoryDTO(cat));
+		}
+		return listDTO;
+		*/
 	}
 }
