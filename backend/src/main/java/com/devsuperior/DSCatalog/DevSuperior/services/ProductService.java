c9e738b9844c28240e0e381940ed0295874f8e10
Edivaldo Almeida <edivaldo.va@gmail.com>
2022-10-08 17:35:11 -0300

    Product insert, update, delete

--- backend/src/main/java/com/devsuperior/DSCatalog/DevSuperior/services/ProductService.java
+++ backend/src/main/java/com/devsuperior/DSCatalog/DevSuperior/services/ProductService.java
@@ -1,6 +1,7 @@
 package com.devsuperior.DSCatalog.DevSuperior.services;
 
 import java.util.Optional;
+
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +12,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.DSCatalog.DevSuperior.dto.CategoryDTO;
 import com.devsuperior.DSCatalog.DevSuperior.dto.ProductDTO;
+import com.devsuperior.DSCatalog.DevSuperior.entities.Category;
 import com.devsuperior.DSCatalog.DevSuperior.entities.Product;
+import com.devsuperior.DSCatalog.DevSuperior.repositories.CategoryRepository;
 import com.devsuperior.DSCatalog.DevSuperior.repositories.ProductRepository;
 import com.devsuperior.DSCatalog.DevSuperior.services.exceptions.DataBaseException;
 import com.devsuperior.DSCatalog.DevSuperior.services.exceptions.ResourceNotFoundException;
@@ -22,6 +26,9 @@ public class ProductService {
 
 	@Autowired
 	private ProductRepository repository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Transactional(readOnly = true)
 	public Page<ProductDTO> findAllPaged(PageRequest pageRequest) {
@@ -51,19 +58,20 @@ public class ProductService {
 	@Transactional
 	public ProductDTO insert(ProductDTO dto) {
 		Product entity = new Product();
-		//entity.setName(dto.getName());
+		copyDtoToEntity(dto, entity);
 		entity = repository.save(entity);
 		return new ProductDTO(entity);
 
 	}
 
+
 	@Transactional
 	public ProductDTO update(Long id, ProductDTO dto) {
 
 		try {
-			Product entity = repository.getOne(id); // instancia uma entity sem persistí-la no banco
-			//entity.setName(dto.getName()); // seta o nome vindo do dto na nova entity provisória
-			entity = repository.save(entity); // salva a entity com o nome atualizado no bd
+			Product entity = repository.getOne(id); 
+			copyDtoToEntity(dto, entity); 
+			entity = repository.save(entity); 
 			return new ProductDTO(entity);
 		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found" + id);
@@ -81,5 +89,22 @@ public class ProductService {
 			catch(DataIntegrityViolationException e) {
 				throw new DataBaseException("Integrit Violation");
 			}
+	}
+	
+	private void copyDtoToEntity(ProductDTO dto, Product entity) {
+
+		entity.setName(dto.getName());
+		entity.setDescription(dto.getDescription());;
+		entity.setDate(dto.getDate());
+		entity.setImgUrl(dto.getImgUrl());
+		entity.setPrice(dto.getPrice());
+		
+		entity.getCategories().clear();
+		for (CategoryDTO catDto: dto.getCategories()) {
+			Category categor = categoryRepository.getOne(catDto.getId());
+			entity.getCategories().add(categor);
+			
+		}
+	
 	}
 }
